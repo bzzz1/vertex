0308618d2654bb2713a57637a2a978528377fc15
Bogdan Kootz <bodyakootz@gmail.com>
2014-11-27 12:59:04 +0200

    Index + Info + Item markup+style update

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -13,146 +13,146 @@
 			<div class="category_icon_block bar_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Барное</br>оборудование</p>
-				<div class="subcategory_block first_line">
-					<div class="subcategory_column">
-						<ul>
-							<li>
-								{{ HTML::link("$env/Барное/Всё", 'Показать всё') }}
-							</li>
-							@foreach ($subcategories['Барное'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Барное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div><!-- subcategory block -->
 			</div>
+			<div class="subcategory_block first_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Барное/Всё", 'Показать всё') }}
+						</li>
+						@foreach ($subcategories['Барное'] as $subcategory)
+							<li>
+								{{ HTML::link("$env/Барное/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
+			</div><!-- subcategory block -->
 			<div class="category_icon_block neutral_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Нейтральное оборудование</p>
-				<div class="subcategory_block first_line">
-					<div class="subcategory_column">
-						<ul>
+			</div>
+			<div class="subcategory_block first_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Нейтральное/Всё", 'Показать всё') }}
+						</li>
+						@foreach ($subcategories['Нейтральное'] as $subcategory)
 							<li>
-								{{ HTML::link("$env/Нейтральное/Всё", 'Показать всё') }}
+								{{ HTML::link("$env/Нейтральное/$subcategory", $subcategory) }}
 							</li>
-							@foreach ($subcategories['Нейтральное'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Нейтральное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block inventory_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Посуда и</br>инвентарь</p>
-				<div class="subcategory_block first_line">
-					<div class="subcategory_column">
-						<ul>
+			</div>
+			<div class="subcategory_block first_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Посуда и инвентарь/Всё", 'Показать всё') }}
+						</li>
+						@foreach ($subcategories['Посуда и инвентарь'] as $subcategory)
 							<li>
-								{{ HTML::link("$env/Посуда и инвентарь/Всё", 'Показать всё') }}
+								{{ HTML::link("$env/Посуда и инвентарь/$subcategory", $subcategory) }}
 							</li>
-							@foreach ($subcategories['Посуда и инвентарь'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Посуда и инвентарь/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block wash_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Посудомоечное оборудование</p>
-				<div class="subcategory_block first_line">
-					<div class="subcategory_column">
-						<ul>
-							<li>
-								{{ HTML::link("$env/Посудомоечное/Всё", 'Показать всё') }}
-							</li>						
-							@foreach ($subcategories['Посудомоечное'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Посудомоечное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
 				</div>
+				<div class="subcategory_block first_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Посудомоечное/Всё", 'Показать всё') }}
+						</li>						
+						@foreach ($subcategories['Посудомоечное'] as $subcategory)
+							<li>
+								{{ HTML::link("$env/Посудомоечное/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block tech_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Технологическое оборудование</p>
-				<div class="subcategory_block second_line">
-					<div class="subcategory_column">
-						<ul>
-							<li>
-								{{ HTML::link("$env/Технологическое/Всё", 'Показать всё') }}
-							</li>						
-							@foreach ($subcategories['Технологическое'] as $subcategory)
-								<li style="display:none">
-									{{ HTML::link("$env/Технологическое/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+			</div>
+			<div class="subcategory_block second_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Технологическое/Всё", 'Показать всё') }}
+						</li>						
+						@foreach ($subcategories['Технологическое'] as $subcategory)
+							<li style="display:none">
+								{{ HTML::link("$env/Технологическое/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block packing_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Упаковочное оборудование</p>
-				<div class="subcategory_block second_line">
-					<div class="subcategory_column">
-						<ul>
+			</div>
+			<div class="subcategory_block second_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Упаковочное/Всё", 'Показать всё') }}
+						</li>						
+						@foreach ($subcategories['Упаковочное'] as $subcategory)
 							<li>
-								{{ HTML::link("$env/Упаковочное/Всё", 'Показать всё') }}
-							</li>						
-							@foreach ($subcategories['Упаковочное'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Упаковочное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+								{{ HTML::link("$env/Упаковочное/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block bread_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Хлебопекарное оборудование</p>
-				<div class="subcategory_block second_line">
-					<div class="subcategory_column">
-						<ul>
+			</div>
+			<div class="subcategory_block second_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Хлебопекарное/Всё", 'Показать всё') }}
+						</li>						
+						@foreach ($subcategories['Хлебопекарное'] as $subcategory)
 							<li>
-								{{ HTML::link("$env/Хлебопекарное/Всё", 'Показать всё') }}
-							</li>						
-							@foreach ($subcategories['Хлебопекарное'] as $subcategory)
-								<li>
-									{{ HTML::link("$env/Хлебопекарное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+								{{ HTML::link("$env/Хлебопекарное/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 			<div class="category_icon_block freez_icon_block" href="index.html">
 				<div class="free_space"></div>
 				<p class="category_icon_title">Холодильное оборудование</p>
-				<div class="subcategory_block second_line">
-					<div class="subcategory_column">
-						<ul>
-							<li>
-								{{ HTML::link("$env/Холодильное/Всё", 'Показать всё') }}
-							</li>						
-							@foreach ($subcategories['Холодильное'] as $subcategory)
-								<li style="display:none">
-									{{ HTML::link("$env/Холодильное/$subcategory", $subcategory) }}
-								</li>
-							@endforeach
-						</ul>
-					</div><!-- brands_column -->
-				</div>
+			</div>
+			<div class="subcategory_block second_line">
+				<div class="subcategory_column">
+					<ul>
+						<li>
+							{{ HTML::link("$env/Холодильное/Всё", 'Показать всё') }}
+						</li>						
+						@foreach ($subcategories['Холодильное'] as $subcategory)
+							<li style="display:none">
+								{{ HTML::link("$env/Холодильное/$subcategory", $subcategory) }}
+							</li>
+						@endforeach
+					</ul>
+				</div><!-- brands_column -->
 			</div>
 
 			<hr class = "main_hr" />
