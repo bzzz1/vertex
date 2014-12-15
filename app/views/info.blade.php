5288634a99eb667ac09237e9782f1ef6762602be
beststrelok <beststrelok@gmail.com>
2014-12-16 00:04:06 +0200

    fix info page

--- app/views/info.blade.php
+++ app/views/info.blade.php
@@ -4,17 +4,15 @@
 
 @section('body')
 	<div class="width_960 catalog_gen">
-		<div class="article_area">
-			@foreach($articles as $article)	
-				<div class="article_preview">
-					<h2 class="article_preview_header">{{ $article->title }}</h2>
-					<div class="article_photo_preview_div">
-						{{ HTML::image("photos/articles/$article->image", 'article', ['class'=>'article_photo_preview']) }}
-					</div>
-					<p class="article_preview_text">{{ $article->body }}</p>
-					{{ Form::button('Читать', ['class'=>'article_button article_button_read']) }}
+		@foreach($articles as $article)	
+			<div class="article_preview">
+				<h2 class="article_preview_header">{{ $article->title }}</h2>
+				<div class="article_photo_preview_div">
+					{{ HTML::image("photos/articles/$article->image", 'article', ['class'=>'article_photo_preview']) }}
 				</div>
-			@endforeach
-		</div>	
+				<p class="article_preview_text">{{ $article->body }}</p>
+				{{ Form::button('Читать', ['class'=>'article_button article_button_read']) }}
+			</div>
+		@endforeach
 	</div><!-- width_960 catalog_gen -->
 @stop
