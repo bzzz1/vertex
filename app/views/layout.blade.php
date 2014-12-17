f11c30d509cdae854ff9ef5d4a9acd81d4b05b21
Iaroslav <beststrelok@gmail.com>
2014-12-17 02:05:37 +0200

    optimize page loading

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,15 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=3, minimum-scale=0.1'>
+	<meta name='viewport' content='width=device-width'>
 	<title>Vertex - Комплексное оснащение баров, ресторанов, кафе, пищевых производств и магазинов</title>
-	{{ HTML::script('js/angular.min.js') }}
 	<link rel="shortcut icon" href="{{ asset('icons/favicon.ico') }}">
-	{{ HTML::style('css/bootstrap.css') }}
+	{{ HTML::style('css/bootstrap.min.css') }}
 	{{ HTML::style('css/reset.css') }}
-	{{ HTML::style('css/text.css') }}
-	{{ HTML::style('css/960_24_col.css') }}
 	{{ HTML::style('css/style.css') }}
+	{{ HTML::script('js/angular.min.js') }}
 	@yield('css')
 	<!--[if lt IE 10]>
 		<!!!!!!!!!!!!!!!script src="{{ asset('js/modernizr_columns.js') }}"></script>
@@ -24,7 +22,7 @@
 		@yield('footer')
 	</div>
 
-	{{ HTML::script('js/jquery.js') }}
+	{{ HTML::script('js/jquery.min.js') }}
 	{{ HTML::script('js/jquery.columnizer.js') }}
 	{{ HTML::script('js/betterContactForm.js') }}
 	{{ HTML::script('js/script.js') }}
