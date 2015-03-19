cbff6cee42391e8847e82b00920cda71776c40da
Bogdan Kootz <bodyakootz@gmail.com>
2015-03-19 14:52:33 +0200

    'contacts_done'

--- app/views/contacts.blade.php
+++ app/views/contacts.blade.php
@@ -2,13 +2,13 @@
 @extends('header')
 @extends('footer')
 
-@section('body')
+@section('body')`
 	<div class="width_960 catalog_gen">
 		
-		<ol class="breadcrumb">
+		{{-- <ol class="breadcrumb">
 		  <li><a href="/">Главная</a></li>
 		  <li class="active">Контакты</li>
-		</ol>
+		</ol> --}}
 		<h2 class="contacts_heading main_cont_heading">Контактная информация</h2>
 		<hr class="main_hr">
 		<div class="contacts_text_block">
