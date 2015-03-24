d29cb378538668f596cab435a35365debf759abc
Bogdan Kootz <bodyakootz@gmail.com>
2015-03-24 21:01:59 +0200

    item_title_full

--- app/views/contacts.blade.php
+++ app/views/contacts.blade.php
@@ -39,8 +39,8 @@
 				</div>
 			</div>
 			<iframe 
-				width="500"
-				height="400" 
+				width="480"
+				height="380" 
 				frameborder="0"
 				style="border:0"
 				src="https://www.google.com/maps/embed/v1/place?q=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C%20%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%20%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C%20%D0%9A%D0%B0%D0%BD%D1%82%D0%B5%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F%20%D1%83%D0%BB%D0%B8%D1%86%D0%B0%2012&key=AIzaSyBR6ruk6PgDAPS4ObXScQCVKtw9a2y0RXw">
