35d238143be44793776d528d3b5f0624f2908556
Iaroslav <beststrelok@gmail.com>
2015-04-01 00:17:51 +0300

    fix images encoding'

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,9 +9,25 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Eloquent::unguard();
-
+		// Eloquent::unguard();
 		// $this->call('UserTableSeeder');
+
+		// $path = "D:\\EASYPH~1.1VC\\data\\localweb\\projects\\vertex\\public\\photos\\articles\\";
+		$path = "D:\\EASYPH~1.1VC\\data\\localweb\\projects\\vertex\\public\\photos\\";
+		$imgs = File::allFiles($path);
+		// $imgs = File::files($path);
+
+		for ($i=0; $i<count($imgs); $i++) {
+			try {
+				$data = $imgs[$i]->getBasename();
+				$name = iconv('utf-8', 'windows-1251', $data);
+				File::move($path.$data, $path.$name);
+			} catch (Exception $e) {
+				echo $i;
+				echo "\n";
+				// echo $e;
+			}
+		}
 	}
 
 }
